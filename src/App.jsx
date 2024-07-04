ad59a8fcc35a94bebdc364fff98f9de3bb7e572c
orrego.sergio <saorregog@gmail.com>
2024-07-04 11:50:08 -0500

    Creacion de componentes CarruselImagenes, TarjetaPublicacion y TarjetaProveedor

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import theme from "./conf/theme";
 
 import EmpresasImpacto from "./componentes/empresas-impacto/EmpresasImpacto";
 import { Categorias } from "./componentes";
+import TarjetaPublicacion from "./componentes/tarjeta-publicacion/TarjetaPublicacion";
+import TarjetaProveedor from "./componentes/tarjeta-proveedor/TarjetaProveedor";
+
+import proveedor from "./componentes/tarjeta-proveedor/proveedor";
+import publicaciones from "./componentes/tarjeta-publicacion/publicaciones";
 
 function App() {
   return (
@@ -12,6 +17,24 @@ function App() {
       <ThemeProvider theme={theme}>
         <EmpresasImpacto />
         <Categorias />
+        <TarjetaProveedor
+          categoria={proveedor.categoria}
+          url={proveedor.url}
+          alt={proveedor.alt}
+          titulo={proveedor.titulo}
+          tipo={proveedor.tipo}
+          ubicacion={proveedor.ubicacion}
+          descripcion={proveedor.descripcion}
+        />
+        {publicaciones.map((publicacion, index) => (
+          <TarjetaPublicacion
+            titulo={publicacion.titulo}
+            fecha={publicacion.fecha}
+            cuerpo={publicacion.cuerpo}
+            imagenes={publicacion.imagenes}
+            key={index}
+          />
+        ))}
       </ThemeProvider>
     </StyledEngineProvider>
   );
--- src/componentes/empresas-impacto/EmpresasImpacto.jsx
+++ src/componentes/empresas-impacto/EmpresasImpacto.jsx
@@ -1,29 +1,24 @@
-import { Box, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import styles from "./EmpresasImpacto.module.css";
 
 function EmpresasImpacto() {
   return (
-    <>
-      <Box
-        borderTop={2}
-        borderBottom={2}
-        borderColor="violeta.main"
-        className={styles.contenedor}
-      >
-        <Typography
-          color="violeta.main"
-          component="h2"
-          className={styles.titulo}
-        >
-          ¿Qué son las empresas de impacto?
-        </Typography>
-        <Typography color="negro.main" component="p" className={styles.cuerpo}>
-          Son organizaciones con un compromiso fundamental con la generación de
-          un impacto positivo en la sociedad y el medio ambiente como parte
-          integral de su modelo de negocio.
-        </Typography>
-      </Box>
-    </>
+    <Box
+      borderTop={2}
+      borderBottom={2}
+      borderColor="violeta.main"
+      className={styles.contenedor}
+    >
+      <Typography color="violeta.main" component="h2" className={styles.titulo}>
+        ¿Qué son las empresas de impacto?
+      </Typography>
+      <Typography color="negro.main" component="p" className={styles.cuerpo}>
+        Son organizaciones con un compromiso fundamental con la generación de un
+        impacto positivo en la sociedad y el medio ambiente como parte integral
+        de su modelo de negocio.
+      </Typography>
+    </Box>
   );
 }
 
