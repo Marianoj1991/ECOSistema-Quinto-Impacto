b96a66cc420b1607ac02a576374f0fefc056545a
Esteban.much <esteban.muchenvicz@gmail.com>
2024-08-23 14:46:50 -0300

    actualizacion de productos y servicios cuatro aleatorios

--- src/paginas/inicio/Inicio.jsx
+++ src/paginas/inicio/Inicio.jsx
@@ -12,7 +12,6 @@ import Typography from "@mui/material/Typography";
 import { useState, useEffect } from "react";
 import { getPublicacionesUser, getProductoSercvivioInicio } from "@/servicios/getAxios";
 import { ordenarPublicacionesPorFecha } from "@/utilidades/ordenarPublicaciones";
-import axios from "axios";
 
 // Estilos CSS
 import styles from "./Inicio.module.css";
@@ -23,42 +22,7 @@ export function Inicio() {
   const [user, setUser] = useState(null);
   const [publicaciones, setPublicaciones] = useState([]);
   const [productsServices, setProductsServices] = useState([]);
-  const [location, setLocation] = useState({ lat: null, lng: null });
   const [error, setError] = useState(null);
-  const [recomendacionesLocales, setRecomendacionesLocales] = useState(false);
-
-  const getPublicacionesLista = async () => {
-    const { data } = await getPublicacionesUser();
-    const sortedPublicaciones = ordenarPublicacionesPorFecha(data);
-    const lasTresPublicacionesRecientes = sortedPublicaciones.slice(0, 3);
-    setPublicaciones(lasTresPublicacionesRecientes);
-  };
-
-  const obtenerUbicacion = async (lat, lng) => {
-    try {
-      const response = await axios.get("https://nominatim.openstreetmap.org/reverse", {
-        params: {
-          lat,
-          lon: lng,
-          format: "json",
-        },
-      });
-      const { address } = response.data;
-      const ciudad = address.city_district;
-      const provincia = address.state;
-      console.log()
-      return { ciudad, provincia };
-    } catch (error) {
-      console.error("Error al obtener la ubicación:", error);
-      return null;
-    }
-  };
-
-  const compararUbicacionConProductos = (ciudad, provincia, productos) => {
-    return productos.filter((producto) => {
-      return producto.ciudad === ciudad || producto.provincia === provincia;
-    });
-  };
 
   const seleccionarCuatroAleatorios = (productos) => {
     return productos.sort(() => 0.5 - Math.random()).slice(0, 4);
@@ -67,42 +31,15 @@ export function Inicio() {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // Obtener productos recomendados
         const { data: productos } = await getProductoSercvivioInicio();
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            async (position) => {
-              const ubicacion = {
-                latitud: position.coords.latitude,
-                longitud: position.coords.longitude,
-              };
-              setLocation(ubicacion);
-
-              const { ciudad, provincia } = await obtenerUbicacion(ubicacion.latitud, ubicacion.longitud);
-              if (ciudad && provincia) {
-                const productosFiltrados = compararUbicacionConProductos(ciudad, provincia, productos);
-
-                if (productosFiltrados.length > 0) {
-                  setProductsServices(productosFiltrados);
-                  setRecomendacionesLocales(true);
-                } else {
-                  setProductsServices(seleccionarCuatroAleatorios(productos));
-                  setRecomendacionesLocales(false);
-                }
-              }
-            },
-            (error) => {
-              console.error("Error en getCurrentPosition:", error);
-              setError(error.message);
-              setProductsServices(seleccionarCuatroAleatorios(productos));
-              setRecomendacionesLocales(false);
-            }
-          );
-        } else {
-          console.error("Geolocation no es compatible con este navegador.");
-          setError("Geolocation is not supported by this browser.");
-          setProductsServices(seleccionarCuatroAleatorios(productos));
-          setRecomendacionesLocales(false);
-        }
+        setProductsServices(seleccionarCuatroAleatorios(productos));
+        
+        // Obtener publicaciones recientes
+        const { data } = await getPublicacionesUser();
+        const sortedPublicaciones = ordenarPublicacionesPorFecha(data);
+        const lasTresPublicacionesRecientes = sortedPublicaciones.slice(0, 3);
+        setPublicaciones(lasTresPublicacionesRecientes);
       } catch (error) {
         console.error("Error al obtener productos o servicios:", error);
         setError(error.message);
@@ -110,7 +47,6 @@ export function Inicio() {
     };
 
     fetchData();
-    getPublicacionesLista();
 
     const userFromSession = sessionStorage.getItem("user");
     if (!userFromSession) {
@@ -128,7 +64,7 @@ export function Inicio() {
       </SeccionTitulo>
       <EmpresasImpacto />
       {user ? null : <InvitacionRegistro />}
-      <Proveedores proveedores={productsServices} recomendacionesLocales={recomendacionesLocales} />
+      <Proveedores proveedores={productsServices} />
       <Categorias />
       <Box className={styles.publicaciones}>
         <Typography className={styles.titulo}>Publicaciones</Typography>
